fbc226b947f1824c51a121b5b0a2854670dd0331
Chris <messiercr@gmail.com>
2019-09-10 17:47:11 -0400

    updated for an additional run:

--- model.py
+++ model.py
@@ -105,5 +105,5 @@ def plot_results(history):
     plt.savefig('assets/cats-v-dogs.png')
 
 if __name__ == "__main__":
-    history = train_model(2)
+    history = train_model(25)
     plot_results(history)
